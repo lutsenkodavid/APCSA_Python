2517e987b4a1a4a2db10efbf6218bc435bca48e6
lutsenkodavid <lutsenkoda@celts.crespi.org>
2024-09-17 00:33:41 -0700

    Add files via upload

---
+++ Chapter 2 Excercise 5.py
@@ -0,0 +1,13 @@
+SPEED = 70
+
+time = 6
+distance = SPEED * time
+print('The distance the car will travel in', time, 'hours is', distance)
+
+time = 10
+distance = SPEED * time
+print('The distance the car will travel in', time, 'hours is', distance)
+
+time = 15
+distance = SPEED * time
+print('The distance the car will travel in', time, 'hours is', distance)
