68da58046a3b0200dadcd633305ec32e18fe36d2
lutsenkodavid <lutsenkoda@celts.crespi.org>
2024-09-24 12:16:49 -0700

    Add files via upload

---
+++ Classwork/Inititals.py
@@ -0,0 +1,19 @@
+#File: DL.py
+#Student: David Lutsenko
+#
+#Date: 01 September 2024
+#Description of Program: THis program was created to type out the initials of DLX with capital letters making up each letter.
+#Github<lutsenkodavid>
+
+def DLX(): 
+    print('DDDDDDDD        LL               XX        XX      ')   
+    print('DDDDDDDDDD      LL                XX      XX       ')  
+    print('DD       DD     LL                 XX    XX        ')  
+    print('DD        DD    LL                  XX  XX         ')  
+    print('DD        DD    LL                   XXXX          ')  
+    print('DD        DD    LL                   XXXX          ') 
+    print('DD        DD    LL                  XX  XX         ')  
+    print('DD       DD     LL                 XX    XX        ') 
+    print('DDDDDDDDDD  ..  LL           ..   XX      XX   ..  ') 
+    print('DDDDDDDD    ..  LLLLLLLLLLLL ..  XX        XX  ..  ')
+DLX()
