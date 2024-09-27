6179fc30a5034a9552d8946a46d883beea4c15c7
lutsenkodavid <lutsenkoda@celts.crespi.org>
2024-09-26 21:22:55 -0700

    Add files via upload

---
+++ homework/Labs/ComputeTaxes.py
@@ -0,0 +1,42 @@
+# File: ComputeTaxes.py
+# Student: David Lutsenko
+#
+# Date: September 26, 2024
+# Description of Program: A program that calculates and displays tax based on marital status and taxable income.
+# GitHub: <david.lutsenko>
+
+def main():
+    print('Welcome to our friendly tax computing program.')
+    print('Please follow the directions.')
+    
+    marital_status = input('Please enter your marital status (s or m): ')
+    
+    if marital_status not in ['s', 'm']:
+        print("Bad marital status entered! Try again later.")
+        return  
+    income = float(input("Enter your taxable income: "))
+    
+    if income < 0:
+        print("Negative income reported! Try again later.")
+
+    if marital_status == 's':
+        if income <= 8000:
+            tax = income * 0.10
+        elif income <= 32000:
+            tax = 800 + (income - 8000) * 0.15
+        else:
+            tax = 4400 + (income - 32000) * 0.25
+        mariage = "Single"
+    else:
+        if income <= 16000:
+            tax = income * 0.10
+        elif income <= 64000:
+            tax = 1600 + (income - 16000) * 0.15
+        else:
+            tax = 8800 + (income - 64000) * 0.25
+        mariage = "Married"
+
+    print('Marital status:',mariage)
+    print('Taxable income:',income)
+    print('Taxes owed:',tax)
+main()
